2dbae1f2705c792a0d1a57b0d27500588345cea2
MariaLivingston <livingstonMmaria@gmail.com>
2022-12-20 05:29:30 +0530

    Stashed and manager work

--- Myjavaregistrationpage.java
+++ Myjavaregistrationpage.java
@@ -1,1 +1,9 @@
-This is my manager work committed.
+<<<<<<< Updated upstream
+This is my manager work committed
+This is my java reg page
+1st day java line
+2nd day java line
+3rd day java line
+4th day java line
+
+>>>>>>> Stashed changes
